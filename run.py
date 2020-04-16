52643bff9c97d1c4394385b739dbae419eb0edb9
Milindh Vijay <milindhvijay@gmail.com>
2020-04-17 00:43:30 +0530

    Admin Window

--- run.py
+++ run.py
@@ -74,5 +74,69 @@ class userwindows:
             self.user_windows=Tk()
             self.user_windows.resizable(False, False)
             self.user_windows.iconbitmap("logo.ico")
-            self.user_windows.title("REGIS")
+            self.user_windows.title("REGISTRX")
+
+
+
+            # 5TH ROW
+            self.tree = ttk.Treeview(self.user_windows,selectmode="browse",column=("column1", "column2", "column3","column4"), show='headings')
+            self.tree.column1("column",width=100,minwidth=100,stretch=NO)
+            self.tree.heading("#1", text="ADMISSION")
+            self.tree.column("column2",width=180,minwidth=180,stretch=NO)
+            self.tree.heading("#2", text="NAME")
+            self.tree.column("column3",width=180,minwidth=180,stretch=NO)
+            self.tree.heading("#3", text="PHONE")
+            self.tree.column("column4",width=450,minwidth=450,stretch=NO)
+            self.tree.heading("#4", text="ADDRESS")
+            self.tree.tag_configure('1', background='ivory2')
+            self.tree.tag_configure('2', background='alice blue')
+            self.tree.grid(row=4,column=0,columnspan=4,sticky=W+E,padx=9,pady=9)
+
+            Label(self.user_windows,text="Search by Part of NAME or Phone No:").grid(row=5,column=0,columnspan=2,pady=9,padx=9,sticky=E)
+            self.search_value = StringVar(self.user_window, value="")
+            Entry(self.user_window,textvariable=self.search_value).grid(row=5,column=2,pady=9,padx=9,sticky=W+E)
+            self.search_button = ttk.Button(self.user_window,text="Search",command=self.search_record)
+            self.search_button.grid(row=5,column,padx=9,pady=9,sticky=W+E)
+
+            self.refresh_button = ttk.Button(self.user_window,text="Refresh",command=self.refresh)
+            self.search_button.grid(row=6,column=2,padx=9,pady=9,sticky=W+E)
+
+
+            self.setup_db()
+            self.user_window.mainloop()
+
+
+class adminwindows:
+
+    sqlite_var = 0 
+    theCursor = 0
+    curItem = 0
+
+    def refresh(self):
+        self.update_tree()
+        self.clear_entries()
+        print("Refreshed")
+
+        def search_record(self):
+        try:
+            self.tree.delete(*self.tree.get_children())
+            self.theCursor.execute("select * from Students where name like ? or phone like  ?",('%'+self.search_value.get()+'%','%'+self.search_value.get()+'%'))
+            self.result = self.theCursor.fetchall()
+            length=str(len(self.result))
+            if(length==0):
+                messagebox.showinfo("Search Results","No results were found, try again using part of name or phone no")
+            if(length!='0'):
+                i=0
+                for row in self.result:
+                    if(i%2==0):
+                        self.tree.insert("",END, values=row,tag='1')
+                    else:
+                        self.tree.insert("",END, values=row,tag='2')  
+                    i=i+1                    
+        except:
+            raise
+            print("Couldn't search Data")
+
+    def reset_db(self):
+        yesno=messagebox.askquestion("RESET DB", "All data in DB will be lost, continue?")
 
